a17762f12ce482feb93f5d7c22554bcaea853d64
francis <francisgtw@gmail.com>
2017-04-20 11:57:48 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # resume
 我的私人简历
+
+[预览](https://szy1000.github.io/resume/dist/)
+
+## 使用方法
+
+``` bash
+git clone git@github.com:szy1000/resume.git
+cd animating-resume
+npm install
+npm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,249 @@
+<template>
+  <div id="app">
+    <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+    <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+  </div>
+</template>
+
+<script>
+  import StyleEditor from './components/StyleEditor'
+  import ResumeEditor from './components/ResumeEditor'
+  import './assets/reset.css'
+
+  export default {
+    name: 'app',
+    components: {
+      StyleEditor,
+      ResumeEditor
+    },
+    data() {
+      return {
+        interval: 50,
+        currentStyle: '',
+        enableHtml: false,
+        fullStyle: [
+          `/*
+* Inspired by http://strml.net/
+* 大家好，我是小沈 博客名：小沈童鞋 
+* 金三银四搞事季，好多公司都在招聘，你是不是也在准备简历呀。
+* 说做就做，我也来写一份简历！
+*/
+
+/* 首先给所有元素加上过渡效果 */
+* {
+  -webkit-transition: all .3s;
+  transition: all .3s;
+}
+/* 白色背景太单调了，我们来点背景 */
+html {
+  color: rgb(222,222,222); background: rgb(63, 82, 99); 
+}
+/* 文字离边框太近了 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 45vw; height: 90vh;
+  font-size: 4em
+}
+/* 代码高亮 */
+.token.selector{ color: rgb(133,153,0); }
+.token.property{ color: rgb(187,137,0); }
+.token.punctuation{ color: yellow; }
+.token.function{ color: rgb(42,161,152); }
+
+/* 加点 3D 效果呗 */
+html{
+  -webkit-perspective: 1000px;
+          perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0; 
+  -webkit-transition: none; 
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
+}
+
+/* 接下来我给自己准备一个编辑器 */
+.resumeEditor{
+  position: fixed; right: 0; top: 0;
+  padding: .5em;  margin: .5em;
+  width: 48vw; height: 90vh; 
+  border: 1px solid;
+  background: white; color: #222;
+  overflow: auto;
+}
+/* 好了，我开始写简历了 */
+
+
+`,
+          `
+/* 这个简历好像差点什么
+ * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
+ * 简单，用开源工具翻译成 HTML 就行了
+ */
+`
+          ,
+          `
+/* 再对 HTML 加点样式 */
+.resumeEditor{
+  padding: 2em;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor ul,.resumeEditor ol{
+  list-style: none;
+}
+.resumeEditor ul> li::before{
+  content: '•';
+  margin-right: .5em;
+}
+.resumeEditor ol {
+  counter-reset: section;
+}
+.resumeEditor ol li::before {
+  counter-increment: section;            
+  content: counters(section, ".") " ";  
+  margin-right: .5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+`],
+        currentMarkdown: '',
+        fullMarkdown: `沈志勇
+----
+
+野蛮生长前端工程师，无背景，无高端学历。
+
+技能
+----
+
+* 三年的前端开发，基础技能该有的应该都有。
+* 当然，也有不会的。但是我不会告诉你，我有Google stackoverflow github这些帮手
+* 领略过模块化开发的强大。深爱npm fis webpack vue 
+* 要说拍黄片(PHP)和java 简单了解过一点前端交互方面的。
+* 团队协作的话 git svn 
+* 爱写博客 爱技术分享
+* Node.js ..... 哈哈 这个我还不会哦！
+
+工作经历
+----
+
+1. 南京大成致远
+2. 上海同道信息
+3. 友熊家
+
+链接
+----
+
+* [GitHub](https://github.com/szy1000)
+* [我的文章](http://blog.csdn.net/u011456552)
+
+> 如果你喜欢这个效果，Fork [我的项目](https://github.com/jirengu-inc/animating-resume)，打造你自己的简历！
+写在最后
+---
+如果您是人事，请仔细比对我的薪资要求与贵公司的预算。
+如果您是技术，麻烦读一下的博客和down一份我的源码。能力的与否也就这些！
+如果不合适，对不起！我的简历打扰到您了，抱歉！
+如果技术，不嫌弃的话，麻烦您给颗star 在走呗！谢谢啊！
+show一下我的藏书
+---
+![my book](https://github.com/szy1000/resume/pic/book.jpg)
+
+`
+      }
+    },
+    created() {
+      this.makeResume()
+    },
+
+    methods: {
+      makeResume: async function () {
+        await this.progressivelyShowStyle(0)
+        await this.progressivelyShowResume()
+        await this.progressivelyShowStyle(1)
+        await this.showHtml()
+        await this.progressivelyShowStyle(2)
+      },
+      showHtml: function () {
+        return new Promise((resolve, reject) => {
+          this.enableHtml = true
+          resolve()
+        })
+      },
+      progressivelyShowStyle(n) {
+        return new Promise((resolve, reject) => {
+          let interval = this.interval
+          let showStyle = (async function () {
+            let style = this.fullStyle[n]
+            if (!style) { return }
+            // 计算前 n 个 style 的字符总数
+            let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+            let prefixLength = length - style.length
+            if (this.currentStyle.length < length) {
+              let l = this.currentStyle.length - prefixLength
+              let char = style.substring(l, l + 1) || ' '
+              this.currentStyle += char
+              if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
+                this.$nextTick(() => {
+                  this.$refs.styleEditor.goBottom()
+                })
+              }
+              setTimeout(showStyle, interval)
+            } else {
+              resolve()
+            }
+          }).bind(this)
+          showStyle()
+        })
+      },
+      progressivelyShowResume() {
+        return new Promise((resolve, reject) => {
+          let length = this.fullMarkdown.length
+          let interval = this.interval
+          let showResume = () => {
+            if (this.currentMarkdown.length < length) {
+              this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
+              let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
+              let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
+              console.log(prevChar)
+              if (prevChar === '\n' && this.$refs.resumeEditor) {
+                this.$nextTick(() => this.$refs.resumeEditor.goBottom())
+              }
+              setTimeout(showResume, interval)
+            } else {
+              resolve()
+            }
+          }
+          showResume()
+        })
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  html {
+    min-height: 100vh;
+  }
+  
+  * {
+    -webkit-transition: all .3s;
+    transition: all .3s;
+  }
+</style>
